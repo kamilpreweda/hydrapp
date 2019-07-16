9acef89476c24fbf4beeed6c392613c9d5e73315
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-16 12:13:22 +0200

    addded buttons js code

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,26 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const counter = document.querySelector('.counter--js');
+const addButton = document.querySelector('.add--js');
+const removeButton = document.querySelector('.remove--js');
+const key = new Date().toISOString().slice(0, 10);
 
-console.log(`Hello world!`);
+addButton.addEventListener('click', () => {
+  if (counter.innerHTML < 9) {
+    counter.innerHTML++
+  }
+  else {
+    alert('Osiągnąłeś dzienny limit szklanek');
+  }
+})
 
+removeButton.addEventListener('click', () => {
+  if (counter.innerHTML > 0) {
+    counter.innerHTML--
+  }
+  else {
+    alert('Nie można usunąć więcej szklanek')
+  }
+})
 
